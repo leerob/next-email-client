264b2aec65bc2de8488eee1b2e50a551f48aaf1a
Lee Robinson <me@leerob.io>
2024-09-30 11:22:22 -0500

    Client side env var

--- app/components/thread-actions.tsx
+++ app/components/thread-actions.tsx
@@ -29,7 +29,7 @@ export function ThreadActions({ threadId }: ThreadActionsProps) {
     initialState
   );
 
-  const isProduction = process.env.VERCEL_ENV === 'production';
+  const isProduction = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
 
   return (
     <TooltipProvider>
